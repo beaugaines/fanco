d157cf381b7b131f19ef9ac73039c53f5ceb20c5
teddyleung <teddy.ph.leung@gmail.com>
2017-07-15 16:01:20 +0000

    Feature complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   authenticated :user do
     root 'dashboard#show', as: :authenticated_root
   end
   root 'home#index'
+  
+  devise_for :users, :controllers => { registrations: 'registrations' }
 end
